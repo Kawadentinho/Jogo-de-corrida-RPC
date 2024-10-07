2262f98949eecc1cd3e48ce66a3131d5b490482b
Kawadentinho <kawakru@gmail.com>
2024-10-07 10:24:10 -0300

    Cena do jogo base criada e referenciado os scripts

--- Assets/Scripts/RaceManager.cs
+++ Assets/Scripts/RaceManager.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.UI;
+using TMPro;
 
 public class RaceManager : MonoBehaviourPunCallbacks
 {
 
     public int totalLaps = 3; // Número total de voltas da corrida
-    public Text lapText; // UI que exibe a volta atual
-    public Text finishText; // UI que exibe a mensagem de final de corrida
+    public TextMeshProUGUI lapText; // UI que exibe a volta atual
+    public TextMeshProUGUI finishText; // UI que exibe a mensagem de final de corrida
     public Transform finishLine; // Referência à linha de chegada
 
     private int playerLap = 0; // Contador de voltas do jogador
